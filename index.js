916ad172ae9d4b76552b643c7869ab55eeb40f44
Josh Martin <joshmartin5001@gmail.com>
2019-02-27 16:55:42 -0500

    Able to grab images from reddit

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const puppeteer = require('puppeteer');
-
+const fetch = require("node-fetch");
 (async () => {
-    const browser = await puppeteer.launch(debugJson)
+    const browser = await puppeteer.launch()
     const page = await browser.newPage()
-    
+
     await page.goto('https://www.reddit.com/r/memes/')
 
-    page.evaluate(() => {
-
+    const tacos = await page.evaluate((arr) => {
+        const filter = Array.prototype.filter
+        const allImages = document.querySelectorAll('img')
+        const memes = filter.call(allImages, (image) => {
+            return image.classList[1] === 'media-element'
+        }).map((data) => data.src.replace('amp;s', 's'))
+        return memes
     })
+    let blobs = []
+    for (taco in tacos) {
+        const url = tacos[taco]
+        blobs.push(await fetch(url).then(r => r.blob()))
+    }
+    await browser.close()
+
 })()
 
-// document.querySelectorAll('img').forEach((data) =>  { 
-//     if (data.classList[1] === 'media-element')
-//         console.log(data.src)})
-    
